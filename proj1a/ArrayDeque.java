b4be77f011910715d25eef128ec64c3c574d2d20
ritap2222 <rita53131@gmail.com>
2022-08-18 20:51:15 +0800

    arrayDeque no resize v1.2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,19 +1,19 @@
-public class ArrayDeque<type> {
-    private type[] items;
+public class ArrayDeque<T> {
+    private T[] items;
     private int size;
     private int prev;
     private int next;
 
 
     public ArrayDeque() {
-        items = (type[]) new Object[8];
+        items = (T[]) new Object[8];
         size = 0;
         prev = 0;
         next = 0;
     }
 
     /**addFirst will change prev*/
-    public void addFirst(type m) {
+    public void addFirst(T m) {
         if (size == 0) {
             items[0] = m;
             next = 1;
@@ -30,7 +30,7 @@ public class ArrayDeque<type> {
     }
 
     /**addLast will change next*/
-    public void addLast(type m) {
+    public void addLast(T m) {
         if (size == 0) {
             items[items.length - 1] = m;
             next = 0;
@@ -59,29 +59,30 @@ public class ArrayDeque<type> {
     }
 
     public void printDeque() {
-        for (int i = 0; i < items.length; i ++) {
+        for (int i = 0; i < items.length; i++) {
             System.out.print(items[i]);
             System.out.print(" ");
         }
     }
 
-    public type removeFirst() {
-        type tempf = items[prev];
+    public T removeFirst() {
+        T tempf = items[prev];
         items[prev] = null;
         prev = prev - 1;
         size = size - 1;
         return tempf;
     }
 
-    public type removeLast() {
-        type templ = items[next];
+    public T removeLast() {
+        T templ = items[next];
         items[next] = null;
         next = next + 1;
         size = size - 1;
         return templ;
     }
 
-    public type get(int index) {
+    public T get(int index) {
         return items[index - 1];
     }
+
 }
