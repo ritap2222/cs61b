16c18ce9e608d56d0ecf2f2b9916d2b9eef8fb25
ritap2222 <rita53131@gmail.com>
2022-09-01 00:21:39 +0800

    test arrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,11 +3,16 @@ public class ArrayDeque<T> {
     private int size;
     private int first;
     private int last;
+    private int remainFirst;
+    private int remainlast;
+    private int reSizeCount = 0;
+    private int remainSize;
+
 
 
 
     public ArrayDeque() {
-        items = (T[]) new Object[8];
+        items = (T[]) new Object[4];
         size = 0;
         first = items.length - 1;
         last = 0;
@@ -15,78 +20,49 @@ public class ArrayDeque<T> {
 
     /**addFirst will change prev*/
     public void addFirst(T m) {
-        /**size = size + 1;
+        size = size + 1;
         if (size > items.length) {
+            remainSize = size - 1;
             reSize();
-            first = items.length - 1;
             items[first] = m;
             first = first - 1;
-            last = size - 1;
         } else {
             items[first] = m;
-            if (first == 0) {
+            first = first - 1;
+            if (first < 0) {
                 first = items.length - 1;
-            } else {
-                first = first - 1;
             }
-        }*/
-
-        if (size == 0) {
-            items[0] = m;
-            size = size + 1;
-            first = items.length - 1;
-            last = 1;
-        } else {
-            items[first] = m;
-            size = size + 1;
-            if (first == 0) {
-                first = items.length - 1;
-            } else {
-                first = first - 1;
-            }
-
         }
     }
 
+
     /**addLast will change next*/
     public void addLast(T m) {
-        /**size = size + 1;
+        size = size + 1;
         if (size > items.length) {
+            remainSize = size - 1;
             reSize();
-            last = size - 1;
             items[last] = m;
             last = last + 1;
-            first = items.length - 1;
         } else {
             items[last] = m;
-            if (last == items.length - 1) {
+            last = last + 1;
+            if (last > items.length - 1) {
                 last = 0;
-            } else {
-                last = last + 1;
-            }
-        }*/
-        if (size == 0) {
-            items[0] = m;
-            size = size + 1;
-            first = items.length - 1;
-            last = 1;
-        } else {
-            items[last] = m;
-            size = size + 1;
-            if(last == items.length - 1) {
-                last = 0;
-            } else {
-                last = last + 1;
             }
         }
-
     }
 
     /**make bigger size*/
     public void reSize() {
-        T[]temp = (T[]) new Object[size * 2];
+        reSizeCount = reSizeCount + 1;
+        T[] temp = (T[]) new Object[size * 2];
         System.arraycopy(items, 0, temp, 0, size - 1);
         items = temp;
+        remainFirst = first + 1;
+        remainlast = last - 1;
+        first = items.length - 1;
+        last = size - 1;
     }
 
     public boolean isEmpty() {
@@ -113,102 +89,99 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
-        size = size - 1;
-        if (first == items.length - 1) {
-            first = 0;
+        if (size == 0) {
+            return null;
         } else {
+            size = size - 1;
             first = first + 1;
+            if (first == items.length && reSizeCount == 0) {
+                first = 0;
+            } else if (first == items.length && reSizeCount > 0) {
+                first = remainFirst;
+            }
+            T temp = items[first];
+            items[first] = null;
+            return temp;
         }
-        T rf = items[first];
-        items[first] = null;
-        return rf;
+
+
     }
 
-    public T removeLast() {
-        size = size - 1;
-        if (last == 0) {
-            last = items.length - 1;
+    public  T removeLast() {
+        if (size == 0) {
+            return null;
         } else {
+            size = size - 1;
             last = last - 1;
+            if (last < 0 && reSizeCount == 0) {
+                last = items.length - 1;
+            } else if (reSizeCount > 0) {
+                last = remainlast;
+                remainlast = remainSize - 1;
+            }
+            T temp = items[last];
+            items[last] = null;
+            return temp;
         }
-        T rl = items[last];
-        items[last] = null;
-        return rl;
+
+
     }
 
-    public T get(int index) {
-        int tempg = first + 1 + index;
-        if (tempg >= items.length - 1) {
-            tempg = tempg - items.length;
-        }
-        return items[tempg];
-    }
+   /**public T get(int index) {
 
-    /**public static void main(String[] args) {
-        ArrayDeque ArrayDeque = new ArrayDeque();
-        /**ArrayDeque.addFirst(1);
-        ArrayDeque.addFirst(2);
-        ArrayDeque.addFirst(3);
-        ArrayDeque.addFirst(4);
-        ArrayDeque.addFirst(5);
-        ArrayDeque.addFirst(6);
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(7);
-        ArrayDeque.addFirst(8);
-        ArrayDeque.addFirst(9);
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(10);
-        ArrayDeque.addFirst(11);
-        ArrayDeque.addFirst(12);
-        ArrayDeque.addFirst(13);
-        ArrayDeque.addFirst(14);
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(15);
-        ArrayDeque.addFirst(16);
-        ArrayDeque.addFirst(17);
-        ArrayDeque.removeLast();
-        ArrayDeque.removeLast();
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(18);
-        ArrayDeque.addFirst(19);
-        ArrayDeque.addFirst(20);
-        ArrayDeque.addFirst(21);
-        ArrayDeque.addFirst(22);
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(23);
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(24);
-        ArrayDeque.addFirst(25);
-        ArrayDeque.addFirst(26);
-        ArrayDeque.addFirst(27);
-        ArrayDeque.addFirst(28);
-        ArrayDeque.removeLast();
-        ArrayDeque.addFirst(29);
-        ArrayDeque.addFirst(30);
-        ArrayDeque.addFirst(31);
-        ArrayDeque.removeLast();
-        ArrayDeque.removeLast();
-        ArrayDeque.removeLast();*/
-
-        /**ArrayDeque.addFirst(0);
-        ArrayDeque.addLast(1);
-        ArrayDeque.addFirst(2);
-        ArrayDeque.addLast(3);
-        ArrayDeque.addFirst(4);
-        ArrayDeque.addLast(5);
-        ArrayDeque.addFirst(6);
-        ArrayDeque.removeFirst();
-        ArrayDeque.addFirst(8);
-        ArrayDeque.removeLast();
-        ArrayDeque.removeFirst();
-        ArrayDeque.removeFirst();
-        ArrayDeque.removeFirst();
-        ArrayDeque.get(0);
-        ArrayDeque.removeLast();
-        ArrayDeque.addLast(15);
-        ArrayDeque.removeFirst();
-        ArrayDeque.addFirst(17);
-        ArrayDeque.removeFirst();
-        ArrayDeque.get(1);
     }*/
+
+
+   public static void main(String[] args) {
+       /**ArrayDeque ArrayDeque = new ArrayDeque();
+       ArrayDeque.addFirst(1);
+       ArrayDeque.addLast(2);
+       ArrayDeque.addFirst(3);
+       ArrayDeque.addFirst(4);
+       ArrayDeque.addLast(5);
+       ArrayDeque.addLast(6);
+       ArrayDeque.removeLast();
+       ArrayDeque.removeLast();
+       ArrayDeque.removeLast();
+       ArrayDeque.removeLast();
+       ArrayDeque.addFirst(7);
+       ArrayDeque.removeLast();//1
+       ArrayDeque.addFirst(7);
+       ArrayDeque.addFirst(8);
+       ArrayDeque.addFirst(9);
+       ArrayDeque.removeLast();//2
+       ArrayDeque.addFirst(10);
+       ArrayDeque.addFirst(11);
+       ArrayDeque.addFirst(12);
+       ArrayDeque.addFirst(13);
+       ArrayDeque.addFirst(14);
+       ArrayDeque.removeLast();//3
+       ArrayDeque.addFirst(15);
+       ArrayDeque.addFirst(16);
+       ArrayDeque.addFirst(17);
+       ArrayDeque.removeLast();//4
+       ArrayDeque.removeLast();//5
+       ArrayDeque.removeLast();//6
+       ArrayDeque.addFirst(18);
+       ArrayDeque.addFirst(19);
+       ArrayDeque.addFirst(20);
+       ArrayDeque.addFirst(21);
+       ArrayDeque.addFirst(22);
+       ArrayDeque.removeLast();//7
+       ArrayDeque.addFirst(23);
+       ArrayDeque.removeLast();//8
+       ArrayDeque.addFirst(24);
+       ArrayDeque.addFirst(25);
+       ArrayDeque.addFirst(26);
+       ArrayDeque.addFirst(27);
+       ArrayDeque.addFirst(28);
+       ArrayDeque.removeLast();//9
+       ArrayDeque.addFirst(29);
+       ArrayDeque.addFirst(30);
+       ArrayDeque.addFirst(31);
+       ArrayDeque.removeLast();//10
+       ArrayDeque.removeLast();//11
+       ArrayDeque.removeLast();*/
+   }
+
 }
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**public class ArrayDequeTest {
+        @Test
+        public void testAdd() {
+            ArrayDeque<Integer> A = new ArrayDeque<Integer>();
+            ArrayDeque.addFirst(10);
+            ArrayDeque.addFirst(11);
+        }
+
+
+    }
+}*/
